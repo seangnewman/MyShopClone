e0157df0a9da74ce3d7d5afc459b8f650be97d80
Sean Newman <seangnewman@msn.com>
2018-02-05 09:53:20 -0700

    Completed data model

---
+++ MyShop/MyShop.Core/Models/BasketItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class BasketItem:BaseEntitiy
+    {
+        public string BasketId { get; set; }
+        public string ProductID { get; set;  }
+
+        public int Quantity { get; set; }
+
+
+        public BasketItem()
+        {
+
+        }
+    }
+}
